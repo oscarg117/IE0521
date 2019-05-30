8031a9cb5a0cdca6180a62434573e816b4151512
oscarg117 <oscarg117@gmail.com>
2019-05-29 20:23:14 -0600

    Implementación Cache Multinivel y Cache Simple.

--- Parte02/src/main.cpp
+++ Parte02/src/main.cpp
@@ -50,26 +50,35 @@ int main(int argc, char * argv []) {
 
   /* Cache variables */
   /* From command line */
-  int tam_cache_kb = 0;
+  int tam_cache_kb_L1 = 0;
+  int n_way_L1 = 0;
+  int tam_cache_kb_L2 = 0;
+  int n_way_L2 = 0;
   int tam_bloque_b = 0;
-  int n_way = 0;
-  cache_optimization opt = RANDOM;
+
+  cache_optimization opt = NONE;
   /* From functions */
-  int tam_tag = 0;
-  int tam_idx = 0;
-  int tam_offset = 0;
+  int tam_tag_L1 = 0;
+  int tam_idx_L1 = 0;
+  int tam_offset_L1 = 0;
+  int tam_tag_L2 = 0;
+  int tam_idx_L2 = 0;
+  int tam_offset_L2 = 0;
   /* From simulation */
-  long long cpu_time = 0;
-  int amat = 0;
-  double overall_miss_rate = 0;
-  double read_miss_rate = 0;
+  double miss_rate_global = 0;
+  double miss_rate_L1 = 0;
+  int misses_L1 = 0;
+  int hits_L1 = 0;
+  double miss_rate_L2 = 0;
+  int misses_L2 = 0;
+  int hits_L2 = 0;
+
+  double miss_rate_L1vc = 0;
+  int misses_L1vc = 0;
+  int hits_L1vc = 0;
+  int hits_vc = 0;
+
   int dirty_evictions = 0;
-  int load_misses = 0;
-  int store_misses = 0;
-  int total_misses = 0;
-  int load_hits = 0;
-  int store_hits = 0;
-  int total_hits = 0;
 
   string line_str =  "--------------------------------------------";
   string line_str0 = "____________________________________________";
@@ -85,7 +94,8 @@ int main(int argc, char * argv []) {
     string arg = argv[i];
     if (arg == "-t") {
       if(i + 1 < argc){
-        tam_cache_kb = stoi(argv[i+1]);
+        tam_cache_kb_L1 = stoi(argv[i+1]);
+        tam_cache_kb_L2 = 4 * tam_cache_kb_L1;
       } else {
         print_usage();
       }
@@ -99,7 +109,8 @@ int main(int argc, char * argv []) {
     }
     if (arg == "-a") {
       if(i + 1 < argc){
-        n_way = stoi(argv[i+1]);
+        n_way_L1 = stoi(argv[i+1]);
+        n_way_L2 = 2 * n_way_L1;
       } else {
         print_usage();
       }
@@ -121,159 +132,207 @@ int main(int argc, char * argv []) {
     }
   }
   // Checks if all parameters are assigned
-  if(tam_cache_kb <= 0 || tam_bloque_b <= 0 || n_way <= 0){
+  if(tam_cache_kb_L1 <= 0 || tam_bloque_b <= 0 || n_way_L1 <= 0){
     print_usage();
   }
-  // Getting tag, index and offset sizes
-  field_size_get( tam_cache_kb, n_way, tam_bloque_b,
-                  &tam_tag, &tam_idx, &tam_offset);
+  // Getting L1 tag, index and offset sizes
+  field_size_get( tam_cache_kb_L1, n_way_L1, tam_bloque_b,
+                  &tam_tag_L1, &tam_idx_L1, &tam_offset_L1);
+
+  // Getting L2 tag, index and offset sizes
+  field_size_get( tam_cache_kb_L2, n_way_L2, tam_bloque_b,
+                  &tam_tag_L2, &tam_idx_L2, &tam_offset_L2);
 
 
-  //Cache instance
-  int n_blocks = 1<<tam_idx;
-  entry cache[n_blocks][n_way];
+  //Cache instances
+  //L1
+  int n_blocks = 1<<tam_idx_L1;
+  entry cache_L1[n_blocks][n_way_L1];
   for (int i = 0; i < n_blocks; i++){
-      for (int j = 0; j < n_way; j++){
-          cache[i][j].valid = false;
-          cache[i][j].dirty = false;
-          cache[i][j].rp_value = n_way - 1;
+      for (int j = 0; j < n_way_L1; j++){
+          cache_L1[i][j].valid = false;
+          cache_L1[i][j].dirty = false;
+          cache_L1[i][j].rp_value = n_way_L1 - 1;
+      }
+  }
+  //L2
+  n_blocks = 1<<tam_idx_L2;
+  entry cache_L2[n_blocks][n_way_L2];
+  for (int i = 0; i < n_blocks; i++){
+      for (int j = 0; j < n_way_L2; j++){
+          cache_L2[i][j].valid = false;
+          cache_L2[i][j].dirty = false;
+          cache_L2[i][j].rp_value = n_way_L2 - 1;
       }
   }
 
   /* Get trace's lines and start your simulation */
   string ctrl = "#";
   bool op;
+  int ic = 0;
   long long address;
-  int ic;
-  int index, tag;
-  operation_result op_result;
-
-  //Start simulation
-  while (ctrl == "#") {
-    ctrl = "N";
-    cin >> ctrl;
-    cin >> op;
-    cin >> hex >> address;
-    cin >> dec >> ic;
-
-    //Gets tag and index  from address
-    address_tag_idx_get(address, tam_tag, tam_idx, tam_offset,
-                        &index, &tag);
-
-    //Keep track of CPU time
-    cpu_time = cpu_time + ic;
-
-
-    //Replacement Policy
-    if(rp == RRIP){
-        srrip_replacement_policy(index, tag,  n_way, op,
-                                 cache[index], &op_result);
-    } else {
-        lru_replacement_policy(  index, tag,  n_way, op,
-                                 cache[index], &op_result);
-    }
-
-    switch (op_result.miss_hit)
-    {
-    case MISS_LOAD:
-        load_misses++;
-        cpu_time+=20;
-        break;
-    case MISS_STORE:
-        store_misses++;
-        break;
-    case HIT_LOAD:
-        load_hits++;
-        break;
-    case HIT_STORE:
-        store_hits++;
-        break;
-    default:
-        break;
-    }
-
-    if(op_result.dirty_eviction){
-        dirty_evictions++;
-    }
-
-    //if(cpu_time > 1000){break;}
-  }//End while
-
-  //Get cache simulation stats
-  total_misses = load_misses + store_misses;
-  total_hits = load_hits + store_hits;
-  overall_miss_rate = double(total_misses)/double(total_misses + total_hits);
-  read_miss_rate = double(load_misses)/double(load_misses + load_hits);
-  amat = 1.0 + overall_miss_rate * 20.0;
+  int index_L1, tag_L1, index_L2, tag_L2;
+  operation_result op_result_L1, op_result_L2;
 
 
   /* Print cache configuration */
 
   cout << endl;
-  cout << "Replacement Policy:\t\t" << rp << endl;
-/*  cout << "Index size:\t" << tam_idx << endl;
-  cout << "Offset size:\t" << tam_offset << endl;
-  cout << "Tag size:\t" << tam_tag << endl;
-*/  cout << endl;
+  cout << "Optimization:\t\t\t" << opt << endl;
+
+  cout << endl;
 
   if(opt == L2){
+    // L1 + L2
+    //Start simulation
+    while (ctrl == "#") {
+      ctrl = "N";
+      cin >> ctrl;
+      cin >> op;
+      cin >> hex >> address;
+      cin >> dec >> ic;
+
+      //Gets tag and index from address
+      address_tag_idx_get(address, tam_tag_L1, tam_idx_L1, tam_offset_L1,
+                          &index_L1, &tag_L1);
+
+      //Se busca dato en L1
+      lru_replacement_policy(  index_L1, tag_L1,  n_way_L1, op,
+                                   cache_L1[index_L1], &op_result_L1);
+
+      if(op_result_L1.miss_hit == HIT_STORE || op_result_L1.miss_hit == HIT_LOAD){
+        // L1 Hit
+        hits_L1++;
+      } else {
+        // L1 Miss
+        misses_L1++;
+
+        //Gets tag and index from address
+        address_tag_idx_get(address, tam_tag_L2, tam_idx_L2, tam_offset_L2,
+                            &index_L2, &tag_L2);
+
+        //Se busca dato en L2
+        lru_replacement_policy(  index_L2, tag_L2,  n_way_L2, op,
+                                     cache_L2[index_L2], &op_result_L2);
+
+        if(op_result_L2.miss_hit == HIT_STORE || op_result_L2.miss_hit == HIT_LOAD){
+          // L2 Hit
+          hits_L2++;
+
+        } else {
+          // L2 Miss
+          misses_L2++;
+
+        }
+
+        if(op_result_L2.dirty_eviction){
+            dirty_evictions++;
+        }
+      }
+
+      //if(cpu_time > 1000){break;}
+    }//End while
+
+    //Get cache simulation stats
+    miss_rate_L1 = double(misses_L1) / double(misses_L1 + hits_L1);
+    miss_rate_L2 = double(misses_L2) / double(misses_L2 + hits_L2);
+    miss_rate_global = miss_rate_L1 * miss_rate_L2;
+
+
     /* Print Statistics */
     cout << line_str << endl;
     cout << "Cache parameters:" << endl;
     cout << line_str << endl;
-    cout << "L1 Cache Size (KB):\t\t" << tam_cache_kb << endl;
-    cout << "L2 Cache Size (KB):\t\t" << tam_cache_kb << endl;
-    cout << "Cache L1 Associativity:\t\t" << n_way << endl;
-    cout << "Cache L2 Associativity:\t\t" << n_way << endl;
+    cout << "L1 Cache Size (KB):\t\t" << tam_cache_kb_L1 << endl;
+    cout << "L2 Cache Size (KB):\t\t" << tam_cache_kb_L2 << endl;
+    cout << "Cache L1 Associativity:\t\t" << n_way_L1 << endl;
+    cout << "Cache L2 Associativity:\t\t" << n_way_L2 << endl;
     cout << "Cache Block Size (bytes):\t" << tam_bloque_b << endl;
     cout << line_str << endl;
     cout << "Simulation results:" << endl;
     cout << line_str << endl;
-    cout << "Overall miss rate:\t\t" << overall_miss_rate << endl;
-    cout << "L1 miss rate:\t\t\t" << read_miss_rate << endl;
-    cout << "L2 miss rate:\t\t\t" << read_miss_rate << endl;
-    cout << "Global miss rate:\t\t" << read_miss_rate << endl;
-    cout << "Misses (L1):\t\t\t" << load_misses << endl;
-    cout << "Hits (L1):\t\t\t" << load_misses << endl;
-    cout << "Misses (L2):\t\t\t" << load_misses << endl;
-    cout << "Hits (L1):\t\t\t" << load_misses << endl;
+    cout << "Global miss rate:\t\t" << miss_rate_global << endl;
+    cout << "L1 miss rate:\t\t\t" << miss_rate_L1 << endl;
+    cout << "L2 miss rate:\t\t\t" << miss_rate_L2 << endl;
+    cout << "Misses (L1):\t\t\t" << misses_L1 << endl;
+    cout << "Hits (L1):\t\t\t" << hits_L1 << endl;
+    cout << "Misses (L2):\t\t\t" << misses_L2 << endl;
+    cout << "Hits (L1):\t\t\t" << hits_L2 << endl;
     cout << "Dirty Evictions (L2):\t\t" << dirty_evictions << endl;
     cout << line_str << endl << endl;
+
   } else if (opt == VC){
+    // L1 + Victim Cache
+
+
+
+
     cout << line_str << endl;
     cout << "Cache parameters:" << endl;
     cout << line_str << endl;
-    cout << "L1 Cache Size (KB):\t\t" << tam_cache_kb << endl;
-    cout << "Cache L1 Associativity:\t\t" << n_way << endl;
+    cout << "L1 Cache Size (KB):\t\t" << tam_cache_kb_L1 << endl;
+    cout << "Cache L1 Associativity:\t\t" << n_way_L1 << endl;
     cout << "Cache Block Size (bytes):\t" << tam_bloque_b << endl;
     cout << line_str << endl;
     cout << "Simulation results:" << endl;
     cout << line_str << endl;
-    cout << "Miss rate (L1 + VC):\t\t" << cpu_time << endl;
-    cout << "Misses (L1 + VC):\t\t" << amat << endl;
-    cout << "Hits (L1 + VC):\t\t\t" << overall_miss_rate << endl;
-    cout << "Victim cache hits:\t\t" << read_miss_rate << endl;
-    cout << "Dirty Evictions:\t\t" << dirty_evictions << endl;
-    cout << line_str << endl << endl;
-  } else if (opt == NONE){
-    cout << line_str << endl;
-    cout << "Cache parameters:" << endl;
-    cout << line_str << endl;
-    cout << "L1 Cache Size (KB):\t\t" << tam_cache_kb << endl;
-    cout << "Cache L1 Associativity:\t\t" << n_way << endl;
-    cout << "Cache Block Size (bytes):\t" << tam_bloque_b << endl;
-    cout << line_str << endl;
-    cout << "Simulation results:" << endl;
-    cout << line_str << endl;
-    cout << "Miss rate (L1):\t\t\t" << cpu_time << endl;
-    cout << "Misses (L1):\t\t\t" << amat << endl;
-    cout << "Hits (L1):\t\t\t" << overall_miss_rate << endl;
-    cout << "Victim cache hits:\t\t" << read_miss_rate << endl;
+    cout << "Miss rate (L1 + VC):\t\t" << miss_rate_L1vc << endl;
+    cout << "Misses (L1 + VC):\t\t" << misses_L1vc << endl;
+    cout << "Hits (L1 + VC):\t\t\t" << hits_L1vc << endl;
+    cout << "Victim cache hits:\t\t" << hits_vc << endl;
     cout << "Dirty Evictions:\t\t" << dirty_evictions << endl;
     cout << line_str << endl << endl;
   } else {
-    cout << "Nunca debería llegar aquí." << endl << endl;
+    // L1
+    // Start simulation
+    while (ctrl == "#") {
+      ctrl = "N";
+      cin >> ctrl;
+      cin >> op;
+      cin >> hex >> address;
+      cin >> dec >> ic;
+
+      //Gets tag and index from address
+      address_tag_idx_get(address, tam_tag_L1, tam_idx_L1, tam_offset_L1,
+                          &index_L1, &tag_L1);
+
+      //Se busca dato en L1
+      lru_replacement_policy(  index_L1, tag_L1,  n_way_L1, op,
+                                   cache_L1[index_L1], &op_result_L1);
+
+      if(op_result_L1.miss_hit == HIT_STORE || op_result_L1.miss_hit == HIT_LOAD){
+        // L1 Hit
+        hits_L1++;
+      } else {
+        // L1 Miss
+        misses_L1++;
+      }
+      if(op_result_L1.dirty_eviction){
+          dirty_evictions++;
+
+      }
+
+      //if(cpu_time > 1000){break;}
+    }//End while
+
+    //Get cache simulation stats
+    miss_rate_L1 = double(misses_L1) / double(misses_L1 + hits_L1);
+
+    cout << line_str << endl;
+    cout << "Cache parameters:" << endl;
+    cout << line_str << endl;
+    cout << "L1 Cache Size (KB):\t\t" << tam_cache_kb_L1 << endl;
+    cout << "Cache L1 Associativity:\t\t" << n_way_L1 << endl;
+    cout << "Cache Block Size (bytes):\t" << tam_bloque_b << endl;
+    cout << line_str << endl;
+    cout << "Simulation results:" << endl;
+    cout << line_str << endl;
+    cout << "Miss rate (L1):\t\t\t" << miss_rate_L1 << endl;
+    cout << "Misses (L1):\t\t\t" << misses_L1 << endl;
+    cout << "Hits (L1):\t\t\t" << hits_L1 << endl;
+    cout << "Dirty Evictions:\t\t" << dirty_evictions << endl;
+    cout << line_str << endl << endl;
   }
 
-return 0;
+  return 0;
 }
